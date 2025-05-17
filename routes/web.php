6eb6d3bc66d78e7087bbb8f43332bf0b33ee69e2
ShaluSafitriArzuf <162129513+ShaluSafitriArzuf@users.noreply.github.com>
2025-05-17 16:35:10 +0700

    Form Movie

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@ Route::get('/', [MovieController::class, 'index']);
 
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
 
-Route::get('/movie/create', [MovieController::class, 'create']);
+Route::get('/movie/create', [MovieController::class, 'create'])->name('movies.create');
+Route::post('/movie', [MovieController::class, 'store'])->name('movies.store');
+Route::get('/', [MovieController::class, 'index'])->name('homepage');
